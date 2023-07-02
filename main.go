def9925a0df7578734911aff69b25e0c6b0c1050
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-07-02 19:28:29 -0400

    main file focus on routing

--- main.go
+++ main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/0xJungleMonkey/timemanagementapp/controllers"
+	"github.com/0xJungleMonkey/timemanagementapp/database"
+	"github.com/gin-gonic/gin"
 )
 
-type Record struct {
-	gorm.Model
-	Start    time.Time
-	Stop     time.Time
-	Duration int
-	Project  string
-}
-
 func main() {
-	//connect database
-	dsn := os.Getenv("DATABASE_DSN")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	fmt.Println(err)
-	if db != nil {
-		fmt.Println("a ...any")
-	} else {
-		fmt.Println(err)
-	}
-	// create record
-	record1 := Record{Start: time.Now(), Stop: time.Now(), Duration: 10, Project: "meinv"}
-	result := db.Create(&record1)
-	fmt.Println(result.RowsAffected)
 
-	// read record
-	var record Record
-	db.First(&record)
-	//In the context of gorm.DB, the First() method does not directly return the retrieved record. Instead, it modifies the provided record2 variable and returns the gorm.DB object itself.
-	//So when you write result2 := db.First(&record2), the result2 variable is of type *gorm.DB, not the record2 variable. That's why result2.ID is undefined because the *gorm.DB type does not have an ID field.
-	//To access the retrieved record's fields, you need to use the record2 variable, which was passed as a reference to the First() method. This method modifies the record2 variable with the fetched record.
-
-	fmt.Println("ID:", record.ID)
-	fmt.Println("Create Time:", record.CreatedAt)
-	fmt.Println("Project:", record.Project)
-
+	r := gin.Default()
+	database.ConnectDatabase()
+	r.GET("/records", controllers.FindRecords)
+	r.GET("/record/:id", controllers.FindRecord)
+	r.POST("/records", controllers.CreateRecord)
+	r.PATCH("/records/:id", controllers.UpdateRecord)
+	r.DELETE("/records/:id", controllers.DeleteRecord)
+	r.Run("localhost:8080")
 }
